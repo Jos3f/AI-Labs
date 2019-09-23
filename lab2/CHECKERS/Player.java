8d135ba2bc2f005d2f13adc51906dd4cc9dab004
Jos3f <Jos3f@users.noreply.github.com>
2019-09-23 14:59:21 +0200

    Add files via upload

--- lab2/CHECKERS/Player.java
+++ lab2/CHECKERS/Player.java
@@ -25,7 +25,75 @@ public class Player {
         }
     }
 
+    /**
+     * Used for repeated state checking
+     */
+    private class State_keeper {
+        public int[] board;
+        public int next_player;
+        public  int[][] table;
 
+        public State_keeper(GameState gameState, int[][] zobrist_init_table){
+            this.next_player = gameState.getNextPlayer();
+            board = new int[gameState.NUMBER_OF_SQUARES];
+            for (int square_index = 0; square_index < gameState.NUMBER_OF_SQUARES; square_index++) {
+                board[square_index] = gameState.get(square_index);
+            }
+            table = zobrist_init_table;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 0;
+            for (int board_index = 0; board_index < board.length; board_index++) {
+                if ((board[board_index]) != 0){ // Zero means empty cell
+                    int piece = board[board_index];
+                    hash = hash ^ table[board_index][piece];
+                }
+            }
+            if (next_player == 2){
+                hash = hash * -1;
+            }
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return true;
+        }
+    }
+
+    private class State_dictionary {
+        HashMap<State_keeper, Integer> dictionary;
+        int[][] zobrist_table; // Zobris initial map
+
+        public State_dictionary() {
+            dictionary = new HashMap<>();
+            // Initialize Zobrist table
+            zobrist_table = new int[32][5];
+            Random rand = new Random();
+            for (int cell_index = 0; cell_index < zobrist_table.length; cell_index++) {
+                for (int piece_type = 1; piece_type < (zobrist_table[0]).length; piece_type++) {
+                    zobrist_table[cell_index][piece_type] = rand.nextInt();
+                }
+            }
+        }
+
+        public int geScore(GameState gameState){
+            State_keeper state = new State_keeper(gameState, zobrist_table);
+            return dictionary.get(state);
+        }
+
+        public void setScore(GameState gameState, int score){
+            State_keeper state = new State_keeper(gameState, zobrist_table);
+            dictionary.put(state, score);
+        }
+
+        public boolean stateExists(GameState gameState){
+            State_keeper state = new State_keeper(gameState, zobrist_table);
+            return dictionary.get(state) != null;
+        }
+    }
 
     //class to hold moves and its heuristic function value
     private class Score {
@@ -153,8 +221,25 @@ public class Player {
             opponent = Constants.CELL_WHITE;
         }
 
-        int depth_minus_one = 6;
-        Score best_scenario = minimaxAlphaBeta(pState, depth_minus_one, Integer.MIN_VALUE, Integer.MAX_VALUE, pState.getNextPlayer(), opponent, pState.getNextPlayer());
+        State_dictionary dict = new State_dictionary();
+
+/*
+        // Testing hashfunction
+        dict.setScore(pState, 1337);
+        boolean stateExist = dict.stateExists(pState);
+        System.err.println("State exists: " + stateExist);
+        stateExist = dict.stateExists(lNextStates.get(0));
+        System.err.println("State exists: " + stateExist);
+
+        System.exit(1);
+*/
+        Score best_scenario = new Score(0,null);
+        int depth_minus_one = 1;
+        while (pDue.timeUntil() > 800000000) {
+            best_scenario = minimaxAlphaBeta(pState, depth_minus_one, Integer.MIN_VALUE, Integer.MAX_VALUE, pState.getNextPlayer(), opponent, pState.getNextPlayer());
+
+            depth_minus_one++;
+        }
 
         /**
          * Here you should write your algorithms to get the best next move, i.e.
