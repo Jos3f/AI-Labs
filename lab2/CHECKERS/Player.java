b21677b1278f3361547c8825bbad49e8a1dd1906
Jos3f <Jos3f@users.noreply.github.com>
2019-09-23 16:29:07 +0200

    Add files via upload

--- lab2/CHECKERS/Player.java
+++ lab2/CHECKERS/Player.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Player {
 
+    public State_dictionary global_dictionary;
+
     private class Pieces_state {
         public int red_pawns;
         public int red_kings;
@@ -32,14 +34,16 @@ public class Player {
         public int[] board;
         public int next_player;
         public  int[][] table;
+        public int depth;
 
-        public State_keeper(GameState gameState, int[][] zobrist_init_table){
+        public State_keeper(GameState gameState, int depth , int[][] zobrist_init_table){
             this.next_player = gameState.getNextPlayer();
             board = new int[gameState.NUMBER_OF_SQUARES];
             for (int square_index = 0; square_index < gameState.NUMBER_OF_SQUARES; square_index++) {
                 board[square_index] = gameState.get(square_index);
             }
             table = zobrist_init_table;
+            this.depth = depth;
         }
 
         @Override
@@ -48,12 +52,14 @@ public class Player {
             for (int board_index = 0; board_index < board.length; board_index++) {
                 if ((board[board_index]) != 0){ // Zero means empty cell
                     int piece = board[board_index];
+                    // System.err.println(piece);
                     hash = hash ^ table[board_index][piece];
                 }
             }
             if (next_player == 2){
                 hash = hash * -1;
             }
+            hash += depth;
             return hash;
         }
 
@@ -70,7 +76,7 @@ public class Player {
         public State_dictionary() {
             dictionary = new HashMap<>();
             // Initialize Zobrist table
-            zobrist_table = new int[32][5];
+            zobrist_table = new int[GameState.NUMBER_OF_SQUARES][7];
             Random rand = new Random();
             for (int cell_index = 0; cell_index < zobrist_table.length; cell_index++) {
                 for (int piece_type = 1; piece_type < (zobrist_table[0]).length; piece_type++) {
@@ -79,18 +85,18 @@ public class Player {
             }
         }
 
-        public int geScore(GameState gameState){
-            State_keeper state = new State_keeper(gameState, zobrist_table);
+        public int getScore(GameState gameState, int depth){
+            State_keeper state = new State_keeper(gameState, depth, zobrist_table);
             return dictionary.get(state);
         }
 
-        public void setScore(GameState gameState, int score){
-            State_keeper state = new State_keeper(gameState, zobrist_table);
+        public void setScore(GameState gameState, int depth,  int score){
+            State_keeper state = new State_keeper(gameState, depth, zobrist_table);
             dictionary.put(state, score);
         }
 
-        public boolean stateExists(GameState gameState){
-            State_keeper state = new State_keeper(gameState, zobrist_table);
+        public boolean stateExists(GameState gameState, int depth){
+            State_keeper state = new State_keeper(gameState, depth, zobrist_table);
             return dictionary.get(state) != null;
         }
     }
@@ -137,8 +143,15 @@ public class Player {
         } else if (player == maxi_player){ //Recursive case for maxi_player
             v.score_val = Integer.MIN_VALUE;
             for (GameState child: possibleStates
-            ) { //opponent and player swith at next level in tree
-                Score v_child = minimaxAlphaBeta(child, depth - 1, alpha, beta, opponent, player, maxi_player);
+            ) { //opponent and player switch at next level in tree
+                Score v_child;
+                if (! global_dictionary.stateExists(child, depth)){
+                    v_child = minimaxAlphaBeta(child, depth - 1, alpha, beta, opponent, player, maxi_player);
+                    global_dictionary.setScore(child, depth, v_child.score_val);
+                } else {
+                    v_child = new Score(0, null);
+                    v_child.score_val = global_dictionary.getScore(child, depth);
+                }
                 if (v_child.score_val > v.score_val){
                     v.score_val = v_child.score_val;
                     v.move = child;
@@ -154,7 +167,14 @@ public class Player {
             v.score_val = Integer.MAX_VALUE;
             for (GameState child: possibleStates
             ) {
-                Score v_child = minimaxAlphaBeta(child, depth - 1, alpha, beta, opponent, player, maxi_player);
+                Score v_child;
+                if (! global_dictionary.stateExists(child, depth)){
+                    v_child = minimaxAlphaBeta(child, depth - 1, alpha, beta, opponent, player, maxi_player);
+                    global_dictionary.setScore(child, depth, v_child.score_val);
+                } else {
+                    v_child = new Score(0, null);
+                    v_child.score_val = global_dictionary.getScore(child, depth);
+                }
                 if (v_child.score_val < v.score_val){
                     v.score_val = v_child.score_val;
                     v.move = child;
@@ -184,19 +204,79 @@ public class Player {
         if (gameState.getNextPlayer() == Constants.CELL_RED){
             opponent = Constants.CELL_WHITE;
         }
-
+/*
         int my_pawns = getNumberOf(gameState, thePlayer, 0);
         int my_kings = getNumberOf(gameState, thePlayer, Constants.CELL_KING);
         int opponent_pawns = getNumberOf(gameState, opponent, 0);
         int opponent_kings = getNumberOf(gameState, opponent, Constants.CELL_KING);
+    */
 
+        int[] value_of_cell = new int[]{
+                1, 1, 1, 1,
+                3, 2, 2, 2,
+                3, 3, 3, 4,
+                5, 4, 4, 4,
+                5, 5, 5, 6,
+                7, 6, 6, 6,
+                7, 7, 7, 8,
+                9, 9, 9, 9,
+        };
+
+        int[] value_of_cell_king = new int[]{
+                2, 2, 2, 2,
+                2, 1, 1, 1,
+                1, 1, 1, 2,
+                2, 1, 1, 1,
+                1, 1, 1, 2,
+                2, 1, 1, 1,
+                1, 1, 1, 2,
+                2, 2, 2, 2,
+        };
+
+
+
+
+        for (int pos = 0; pos < GameState.NUMBER_OF_SQUARES; pos++) {
+            if ((gameState.get(pos) & thePlayer) > 0){ // My piece
+                if ((gameState.get(pos) & Constants.CELL_KING) > 0){ // My king piece
+                    score += 8 * value_of_cell_king[pos];
+                } else { // My pawn piece
+                    score += 4 * value_of_cell[pos];
+
+                }
+
+            } else if ((gameState.get(pos) & opponent) > 0){ // opponent piece
+                if ((gameState.get(pos) & Constants.CELL_KING) > 0){ // opponent king piece
+                    score -= 8 * value_of_cell_king[(GameState.NUMBER_OF_SQUARES - 1) - pos];
+
+                } else { // opponent pawn piece
+                    score -= 4 * value_of_cell[(GameState.NUMBER_OF_SQUARES - 1) - pos];
+
+                }
+            }
+        }
+/*
         score+= 4 * my_pawns;
         score+= 8 * my_kings;
         score-= 4 * opponent_pawns;
         score-= 8 * opponent_kings;
+*/
+
 
         return new Score(score, gameState);
     }
+
+    /**
+     * Function for calculating average distances between the pieces
+     */
+    public double avgDistance(GameState gameState){
+        ArrayList<Integer> my_pieces = new ArrayList<>(); // Fill with pos of my pieces
+        ArrayList<Integer> opponet_pieces = new ArrayList<>(); // Fill with pos of opponent pieces
+        double dist = 0;
+         return dist;
+    }
+
+
     /**
      * Performs a move
      *
@@ -225,17 +305,27 @@ public class Player {
 
 /*
         // Testing hashfunction
-        dict.setScore(pState, 1337);
-        boolean stateExist = dict.stateExists(pState);
+        dict.setScore(pState, 10,1337);
+        boolean stateExist = dict.stateExists(pState, 10);
         System.err.println("State exists: " + stateExist);
-        stateExist = dict.stateExists(lNextStates.get(0));
+        stateExist = dict.stateExists(lNextStates.get(0), 10);
         System.err.println("State exists: " + stateExist);
+
+        int[] a = new int[]{1,2};
+        int[] b = new int[]{1,2};
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(a.hashCode(), 123);
+        System.err.println("Key b gives: " + map.get(b.hashCode()));
 
         System.exit(1);
 */
+
         Score best_scenario = new Score(0,null);
         int depth_minus_one = 1;
-        while (pDue.timeUntil() > 800000000) {
+
+        while (pDue.timeUntil() > 750000000) {
+            global_dictionary = new State_dictionary();
+
             best_scenario = minimaxAlphaBeta(pState, depth_minus_one, Integer.MIN_VALUE, Integer.MAX_VALUE, pState.getNextPlayer(), opponent, pState.getNextPlayer());
 
             depth_minus_one++;
